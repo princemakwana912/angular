e7d71ed89509a5aa5db33af148a45959304297d8
Prince Makwana <122629826+princemakwana912@users.noreply.github.com>
2023-06-29 17:03:29 +0530

    Add files via upload
    1Authority

---
+++ AuthorityDemo/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthCallbackComponent } from './core/components/auth-callback/auth-callback.component';
+import { MasterComponent } from './core/components/master/master.component';
+import { AuthGuard } from './core/services/guard/auth.guard';
+
+const routes: Routes = [
+  {
+    path: 'auth-callback',
+    component: AuthCallbackComponent
+  },
+  {
+    path: '',
+    component: MasterComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+        canActivate: [AuthGuard]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
